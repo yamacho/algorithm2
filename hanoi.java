ae70e25c8cfc34376edfc1affe4b8067ecd9b669
yamacho <yamacho@outlook.com>
2017-01-27 13:45:53 +0900

    hanoi.java: piles 変数を追加

--- hanoi.java
+++ hanoi.java
@@ -2,6 +2,13 @@
 import java.util.*;
 
 class Main {
+    /*
+      piles    : 3 本の杭を list に格納
+      名前なし : 杭は LinkedList で円盤を格納
+      名前なし : 円盤の大きさを整数で表す
+    */
+    static List<LinkedList<Integer>> piles;
+
     public static void main(String[] args) {
         int n = 100;
 
