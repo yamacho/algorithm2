7f48c4a4d4473378d15950f87093b42675afd4fd
yamacho <yamacho@outlook.com>
2017-01-27 13:41:01 +0900

    hanoi.java: main メソッドを追加

--- hanoi.java
+++ hanoi.java
@@ -1,4 +1,10 @@
 // ハノイの塔の解説 - 初期設定、出力
 import java.util.*;
 
-class Main {}
+class Main {
+    public static void main(String[] args) {
+        int n = 100;
+
+        System.out.println(n);
+    }
+}
